95dc2681fb6319d5faae1eb943592eac123178c2
Christopher Long <galizur@gmail.com>
2015-01-10 18:13:20 -0500

    Pull rosters by year and division.

--- ncaa_pbp/scrapers/ncaa_team_rosters.rb
+++ ncaa_pbp/scrapers/ncaa_team_rosters.rb
@@ -5,101 +5,133 @@ require 'csv'
 require 'nokogiri'
 require 'open-uri'
 
+nthreads = 10
+
 base_sleep = 0
 sleep_increment = 3
 retries = 4
 
-ncaa_teams = CSV.open("csv/ncaa_teams.csv","r",{:col_sep => "\t", :headers => TRUE})
-ncaa_team_rosters = CSV.open("csv/ncaa_team_rosters.csv","w",{:col_sep => "\t"})
-
-#http://stats.ncaa.org/team/roster/11540?org_id=2
-
-# Header for team file
-
-ncaa_team_rosters << ["year","year_id","team_id","team_name","jersey_number","player_id","player_name","player_url","position","height","class_year","games_played","games_started"]
+year = ARGV[0].to_i
+division = ARGV[1].to_i
 
 # Base URL for relative team links
 
 base_url = 'http://stats.ncaa.org'
 
-sleep_time = base_sleep
+roster_xpath = '//*[@id="stat_grid"]/tbody/tr'
 
-ncaa_teams.each do |team|
+teams = CSV.read("csv/ncaa_teams_#{year}_#{division}.csv","r",{:col_sep => "\t", :headers => TRUE})
+ncaa_team_rosters = CSV.open("csv/ncaa_team_rosters_#{year}_#{division}.csv","w",{:col_sep => "\t"})
 
-  year = team[0]
-  year_id = team[1]
-  team_id = team[2]
-  team_name = team[3]
+# Header for team file
 
-  roster_xpath = '//*[@id="stat_grid"]/tbody/tr'
+ncaa_team_rosters << ["year", "year_id", "division_id",
+                      "team_id", "team_name", "jersey_number",
+                      "player_id", "player_name", "player_url",
+                      "position", "height", "class_year",
+                      "games_played", "games_started"]
 
-  team_roster_url = "http://stats.ncaa.org/team/roster/#{year_id}?org_id=#{team_id}"
+# Get team IDs
 
-  print "Sleep #{sleep_time} ... "
-  sleep sleep_time
+#teams = []
+#ncaa_teams.each do |team|
+#  teams << team
+#end
 
-  found_players = 0
-  missing_id = 0
+n = teams.size
 
-  tries = 0
-  begin
-    doc = Nokogiri::HTML(open(team_roster_url))
-  rescue
-    sleep_time += sleep_increment
-    print "sleep #{sleep_time} ... "
-    sleep sleep_time
-    tries += 1
-    if (tries > retries)
-      next
-    else
-      retry
-    end
-  end
+tpt = (n.to_f/nthreads.to_f).ceil
 
-  sleep_time = base_sleep
+threads = []
 
-  print "#{year} #{team_name} ..."
+teams.each_slice(tpt).with_index do |teams_slice,i|
 
-  doc.xpath(roster_xpath).each do |player|
+  threads << Thread.new(teams_slice) do |t_teams|
 
-    row = [year, year_id, team_id, team_name]
-    player.xpath("td").each_with_index do |element,i|
-      case i
-      when 1
-        player_name = element.text.strip
+    t_teams.each_with_index do |team,j|
 
-        link = element.search("a").first
-        if (link==nil)
-          missing_id += 1
-          link_url = nil
-          player_id = nil
-          player_url = nil
+      sleep_time = base_sleep
+
+      year = team["year"]
+      year_id = team["year_id"]
+      team_id = team["team_id"]
+      team_name = team["team_name"]
+
+      team_roster_url = "http://stats.ncaa.org/team/roster/#{year_id}?org_id=#{team_id}"
+
+      #print "Sleep #{sleep_time} ... "
+      sleep sleep_time
+
+      found_players = 0
+      missing_id = 0
+
+      tries = 0
+      begin
+        doc = Nokogiri::HTML(open(team_roster_url))
+      rescue
+        sleep_time += sleep_increment
+        #print "sleep #{sleep_time} ... "
+        sleep sleep_time
+        tries += 1
+        if (tries > retries)
+          next
         else
-          link_url = link.attributes["href"].text
-          parameters = link_url.split("/")[-1]
+          retry
+        end
+      end
 
-          # player_id
+      sleep_time = base_sleep
 
-          player_id = parameters.split("=")[2]
+      print "#{i} #{year} #{team_name} ..."
 
-          # opponent URL
+      doc.xpath(roster_xpath).each do |player|
 
-          player_url = base_url+link_url
+        row = [year, year_id, division, team_id, team_name]
+        player.xpath("td").each_with_index do |element,k|
+          case k
+          when 1
+            player_name = element.text.strip
+
+            link = element.search("a").first
+            if (link==nil)
+              missing_id += 1
+              link_url = nil
+              player_id = nil
+              player_url = nil
+            else
+              link_url = link.attributes["href"].text
+              parameters = link_url.split("/")[-1]
+
+              # player_id
+
+              player_id = parameters.split("=")[2]
+
+              # opponent URL
+
+              player_url = base_url+link_url
+            end
+
+            found_players += 1
+            row += [player_id, player_name, player_url]
+          else
+            field_string = element.text.strip
+
+            row += [field_string]
+          end
         end
 
-        found_players += 1
-        row += [player_id, player_name, player_url]
-      else
-        field_string = element.text.strip
-
-        row += [field_string]
+        ncaa_team_rosters << row
+    
       end
+
+      print " #{found_players} players, #{missing_id} missing ID\n"
+
     end
 
-    ncaa_team_rosters << row
-    
   end
 
-  print " #{found_players} players, #{missing_id} missing ID\n"
-
 end
+
+threads.each(&:join)
+
+ncaa_team_rosters.close
