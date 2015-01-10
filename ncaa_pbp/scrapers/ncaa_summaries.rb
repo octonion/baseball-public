ce24a89374f0f683c60f4ff68037e3361004ab63
Christopher Long <galizur@gmail.com>
2015-01-10 18:13:45 -0500

    Pull team and player summaries by year and division.

--- ncaa_pbp/scrapers/ncaa_summaries.rb
+++ ncaa_pbp/scrapers/ncaa_summaries.rb
@@ -9,11 +9,12 @@ base_sleep = 0
 sleep_increment = 3
 retries = 4
 
-ncaa_teams = CSV.open("csv/ncaa_teams.csv","r",{:col_sep => "\t", :headers => TRUE})
-ncaa_player_summaries = CSV.open("csv/ncaa_player_hitting_summaries.csv","w",{:col_sep => "\t"})
-ncaa_team_summaries = CSV.open("csv/ncaa_team_hitting_summaries.csv","w",{:col_sep => "\t"})
+year = ARGV[0]
+division = ARGV[1]
 
-#http://stats.ncaa.org/team/roster/11540?org_id=2
+ncaa_teams = CSV.read("csv/ncaa_teams_#{year}_#{division}.csv","r",{:col_sep => "\t", :headers => TRUE})
+ncaa_player_summaries = CSV.open("csv/ncaa_player_hitting_summaries_#{year}_#{division}.csv","w",{:col_sep => "\t"})
+ncaa_team_summaries = CSV.open("csv/ncaa_team_hitting_summaries_#{year}_#{division}.csv","w",{:col_sep => "\t"})
 
 # Headers for files
 
@@ -22,15 +23,16 @@ ncaa_team_summaries = CSV.open("csv/ncaa_team_hitting_summaries.csv","w",{:col_s
 #AB	R	H	2B	3B	TB	HR	RBI	BB	HBP
 #SF	SH	K	DP	SB	CS	Picked
 
-ncaa_player_summaries << [
-"year", "year_id", "team_id", "team_name",
+ncaa_player_summaries << ["year", "year_id", "division_id",
+"team_id", "team_name",
 "jersey_number", "player_id", "player_name", "player_url", "class_year",
 "position",
 "gp", "gs", "g", "ba", "obp", "slg",
 "ab", "r", "h", "d", "t", "tb", "hr", "rbi", "bb", "hbp",
 "sf", "sh", "k", "dp", "sb", "cs", "picked"]
 
-ncaa_team_summaries << ["year","year_id","team_id","team_name",
+ncaa_team_summaries << ["year", "year_id", "division_id",
+"team_id", "team_name",
 "jersey_number", "player_name", "class_year", "position",
 "gp", "gs", "g", "ba", "obp", "slg",
 "ab", "r", "h", "d", "t", "tb", "hr", "rbi", "bb", "hbp",
@@ -44,10 +46,10 @@ sleep_time = base_sleep
 
 ncaa_teams.each do |team|
 
-  year = team[0]
-  year_id = team[1]
-  team_id = team[2]
-  team_name = team[3]
+  year = team["year"]
+  year_id = team["year_id"]
+  team_id = team["team_id"]
+  team_name = team["team_name"]
 
   players_xpath = '//*[@id="stat_grid"]/tbody/tr'
 
@@ -82,7 +84,7 @@ ncaa_teams.each do |team|
 
   doc.xpath(players_xpath).each do |player|
 
-    row = [year, year_id, team_id, team_name]
+    row = [year, year_id, division, team_id, team_name]
     player.xpath("td").each_with_index do |element,i|
       case i
       when 1
