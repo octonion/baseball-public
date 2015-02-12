e9f20f985488de9a1d25d57f2c704d8012076af9
Christopher Long <galizur@gmail.com>
2015-02-12 00:48:24 -0500

    NAIA initial import.

---
+++ naia/naia_conferences.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+
+require 'csv'
+require 'mechanize'
+
+agent = Mechanize.new{ |agent| agent.history.max_size=0 }
+agent.user_agent = 'Mozilla/5.0'
+
+base_url = 'http://www.dakstats.com/WebSync/Pages/MultiTeam/Division.aspx?association=10&sg=MBA&division=NAIMBA1'
+
+#'&sea=NAIMBA_2014'
+path = '//*[(@id = "ctl00_websyncContentPlaceHolder_conferenceDataList")]//a'
+
+first_year = 2004
+last_year = 2015
+
+conference_header = ["conference_id", "year", "conference_name"]
+
+(first_year..last_year).each do |year|
+
+  conferences = CSV.open("csv/naia_conferences_#{year}.csv", "w")
+  conferences << conference_header
+
+  url = base_url+"&sea=NAIMBA_#{year}"
+
+  begin
+    page = agent.get(url)
+  rescue
+    print "  -> error, retrying\n"
+    retry
+  end
+
+  page.parser.xpath(path).each do |a|
+    href = a.attribute("href")
+    conferences << [a.attribute("href").text.split("=")[-1], year, a.text.strip]
+  end
+
+  conferences.close
+
+end
+
+
+
---
+++ naia/naia_games.rb
@@ -0,0 +1,129 @@
+#!/usr/bin/env ruby
+
+require "csv"
+require "mechanize"
+
+#game_date,conference_id,conference_name,home_team,home_team_site,away_team,away_team_site,time/score,boxscore_link,webcast_text,webcast_javascript
+
+agent = Mechanize.new
+agent.user_agent = 'Mozilla/5.0'
+
+#path = '//*[contains(concat( " ", @class, " " ), concat( " ", "d8", " " ))]'
+
+path = '//*[@id="ctl00_websyncContentPlaceHolder_scheduleDataList"]//tr'
+
+base="http://www.dakstats.com/WebSync/Pages/Conference/ConferenceSchedule.aspx"
+
+header = ["game_date", "home_name", "home_url", "home_id",
+          "away_name", "away_url", "away_id",
+          "game_score", "boxscore_url",
+          "association_id", "sport_id", "season_id",
+          "conference_id", "game_id",
+          "webcast_js", "tournament_js", "gamebook_js"]
+
+association="10"
+sg="MBA"
+
+#&sea=NAIMBA_2011&conference=NAIMBA1_AMEC"
+
+first_year = 2004
+last_year = 2014
+
+(first_year..last_year).each do |year|
+
+  conferences = CSV.read("csv/naia_conferences_#{year}.csv",
+                         { :headers => TRUE })
+
+  games = CSV.open("csv/naia_games_#{year}.csv","w")
+  games << header
+
+  conferences.each do |conference|
+
+    conference_id = conference["conference_id"]
+    year = conference["year"]
+    conference_name = conference["conference_name"]
+
+    sea = "NAIMBA_#{year}"
+
+    print "#{year} - #{conference_name}\n"
+    url = "#{base}?association=#{association}&sg=#{sg}&sea=#{sea}&conference=#{conference_id}"
+
+    begin
+      page = agent.get(url)
+    rescue
+      print "Error: Retrying\n"
+      retry
+      #print "missing\n"
+    end
+
+    rows = []
+
+    game_date = nil
+    page.parser.xpath(path).each do |element|
+
+      ec = element.parent.attr("class")
+
+      if (ec == "subHeaderTable")
+        game_date = element.text.strip
+        next
+      elsif (ec == nil)
+        next
+      end
+
+      row = [game_date]
+      element.xpath("td").each_with_index do |td,i|
+
+        text = td.text.strip rescue nil
+        a = td.xpath("a").first
+
+        if not(a==nil)
+          href = a.attr("href")
+        else
+          href = nil
+        end
+
+        case i
+        when 0,1
+          row += [text,href]
+          if not(href==nil)
+            p = CGI.parse(URI.parse(href).query)
+            team_id = p["team"][0]
+            row += [team_id]
+          else
+            row += [nil]
+          end
+
+        when 2
+          row += [text,href]
+          if not(href==nil)
+            p = CGI.parse(URI.parse(href).query)
+            association_id = p["association"][0]
+            sport_id = p["sg"][0]
+            season_id = p["sea"][0]
+            conference_id = p["conference"][0]
+            game_id = p["compID"][0]
+            row += [association_id, sport_id, season_id, conference_id, game_id]
+          else
+            row += [nil, nil, nil, nil, nil]
+          end
+
+        when 3
+          td.xpath("div/input").each do |input|
+            onclick = input.attr("onclick")
+            row += [onclick]
+          end
+
+        end
+
+      end
+
+      games << row
+
+    end
+
+  end
+  games.close
+
+end
+
+
