d5952cb9a2b848c0139cab90ec2111455a6ba936
Christopher D. Long <galizur@gmail.com>
2017-05-09 04:40:29 -0400

    Repull divisions

--- ncaa/loaders/load_ncaa_divisions.sql
+++ ncaa/loaders/load_ncaa_divisions.sql
@@ -17,15 +17,15 @@ create table ncaa.schools_divisions (
 
 copy ncaa.schools_divisions from '/tmp/ncaa_divisions.csv' with delimiter as ',' csv quote as '"';
 
--- Temporary fix for 2015
+-- Temporary fix for 2017
 
 insert into ncaa.schools_divisions
 (sport_code,school_name,school_id,pulled_name,javascript,year,div_id,school_year,sport,division)
 (
-select sport_code,school_name,school_id,pulled_name,javascript,2015,div_id,school_year,sport,division
+select sport_code,school_name,school_id,pulled_name,javascript,2017,div_id,school_year,sport,division
 from ncaa.schools_divisions
-where year=2014
-and (school_id,2015) not in
+where year=2016
+and (school_id,2017) not in
 (select school_id,year from ncaa.schools_divisions)
 );
 
