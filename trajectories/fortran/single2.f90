e0a89afe2cb0483962b2783e2e19aad248f70539
Christopher D. Long <galizur@gmail.com>
2018-02-15 19:01:48 -0500

    Initial refactor.

--- trajectories/fortran/single2.f90
+++ trajectories/fortran/single2.f90
@@ -317,9 +317,12 @@ subroutine dist(range,time)
   parameter(nterms=8,pi=3.1415926,twopi=2.*pi,rad=pi/180.)
   parameter(npts=1000)
   common/path/t(npts),x(nterms,npts)
+  
+  ! Replace with a while-do loop
   do i=1,npts
      if(x(3,i).le.0..and.i.gt.1) go to 250
   end do
+  
   ! linear interpolation to get range
 250 frac=x(3,i-1)/(x(3,i-1)-x(3,i))
   d1=sqrt(x(2,i-1)**2+x(1,i-1)**2)
