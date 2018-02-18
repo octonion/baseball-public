c0bc3540a5ed550265ec70be5ba1c5620c778c72
Christopher D. Long <galizur@gmail.com>
2018-02-18 14:12:15 -0500

    Better function wrapper.

--- trajectories/python/trajectory.py
+++ trajectories/python/trajectory.py
@@ -10,30 +10,54 @@ lib.trajectory_.argtypes = [
     POINTER(c_float),POINTER(c_float),POINTER(c_float)
 ]
 
+def trajectory(cd0,cddot,cdspin,w0,theta0,tau0,rhodata,vdata,thetadata,vwind):
+
+    cd0 = c_float(cd0)
+    cddot = c_float(cddot)
+    cdspin = c_float(cdspin)
+    w0 = c_float(w0)
+    theta0 = c_float(theta0)
+    tau0 = c_float(tau0)
+
+    rhodata = c_float(rhodata)
+    vdata = c_float(vdata)
+    thetadata = c_float(thetadata)
+    vwind = c_float(vwind)
+
+    spin_rpm = c_float(0.0)
+    sfact = c_float(0.0)
+    cdi = c_float(0.0)
+    distance = c_float(0.0)
+    tof = c_float(0.0)
+
+    lib.trajectory_(
+        byref(cd0),byref(cddot),byref(cdspin),
+        byref(w0),byref(theta0),byref(tau0),
+        byref(rhodata),byref(vdata),byref(thetadata),byref(vwind),
+        byref(spin_rpm),byref(sfact),byref(cdi),byref(distance),byref(tof))
+
+    return spin_rpm.value, sfact.value, cdi.value, distance.value, tof.value
+
 # Example from Alan's parameters.csv
 
-cd0 = c_float(0.4103)
-cddot = c_float(0.0044)
-cdspin = c_float(0.2043)
-w0 = c_float(2069.4111)
-theta0 = c_float(7.2023)
-tau0 = c_float(25.0000)
+cd0 = 0.4103
+cddot = 0.0044
+cdspin = 0.2043
+w0 = 2069.4111
+theta0 = 7.2023
+tau0 = 25.0000
 
 # Example from Alan's trajectory_input.csv
 
-rhodata = c_float(1.19)
-vdata = c_float(91.0)
-thetadata = c_float(27.5)
-vwind = c_float(2.0)
+rhodata = 1.19
+vdata = 91.0
+thetadata = 27.5
+vwind = 2.0
 
 # These values are returned
 
-spin_rpm = c_float(0.0)
-sfact = c_float(0.0)
-cdi = c_float(0.0)
-distance = c_float(0.0)
-tof = c_float(0.0)
+spin_rpm,sfact,value,distance,tof = trajectory(
+    cd0,cddot,cdspin,w0,theta0,tau0,
+    rhodata,vdata,thetadata,vwind)
 
-lib.trajectory_(byref(cd0),byref(cddot),byref(cdspin),byref(w0),byref(theta0),byref(tau0),byref(rhodata),byref(vdata),byref(thetadata),byref(vwind),byref(spin_rpm),byref(sfact),byref(cdi),byref(distance),byref(tof))
-
-print("distance = ",distance.value,", time-of-flight = ",tof.value)
+print("distance = ",distance,", time-of-flight = ",tof)
